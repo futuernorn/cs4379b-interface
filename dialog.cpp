76eb2bbde0f0bb85fbfe22f02c7e35bf8ff91c0d
Jeffrey Hogan <jeff.hogan1@gmail.com>
2015-02-14 14:49:23 -0600

    Added first update slot

--- dialog.cpp
+++ dialog.cpp
@@ -49,7 +49,7 @@
     for (int i = 0; i < NUM_BUTTONS; ++i) {
       if (methodButtons[i]->isChecked()) {
         //QString value = i;
-        methodEdit->setText(QString::fromUtf8(methodLabels[i]));
+        methodEdit->setText(QString::fromStdString(methodLabels[i]));
       }
     }
   }
--- dialog.cpp~
+++ dialog.cpp~
@@ -49,7 +49,7 @@
     for (int i = 0; i < NUM_BUTTONS; ++i) {
       if (methodButtons[i]->isChecked()) {
         //QString value = i;
-        methodEdit->setText(QString(methodLabels[i]));
+        methodEdit->setText(QString::fromUtf8(methodLabels[i]));
       }
     }
   }
