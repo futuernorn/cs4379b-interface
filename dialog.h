ab5ff1cd39326bfee63176cb0da42ba710fee507
Jeffrey Hogan <jeff.hogan1@gmail.com>
2015-02-15 16:12:16 -0600

    Final submission for 2/15/15

--- dialog.h
+++ dialog.h
@@ -1,68 +1,82 @@
-  #ifndef DIALOG_H
- #define DIALOG_H
-
- #include <QDialog>
- #include <QRadioButton>
- #include <QSlider>
-#include <QDial>
- #include <QProgressBar>
-#include <QLCDNumber>
-#include <QComboBox>
-#include <QList>
-#include <QTabBar>
-#include <QPushButton>
- class QAction;
- class QDialogButtonBox;
- class QGroupBox;
- class QLabel;
- class QLineEdit;
- class QMenu;
- class QMenuBar;
- class QPushButton;
- class QTextEdit;
-
-#include <QString>
- class Dialog : public QDialog
- {
-     Q_OBJECT
-
- public:
-     Dialog();
-  private slots:
-    void UpdateMethodLineEdit();
-    void UpdateSizeLCD();
- private:
-//void UpdateMethodLineEdit();
-     void createMenu();
-     void createMethodGroupBox();
-     void createImageSizeGroupBox();
-     void createImageBrightnessGroupBox();
-  void createColorGroupBox();
-     void createLandscapeGroupBox();
-    void setMethodEdit(int id);
-
-     enum { NumGridRows = 3, NUM_BUTTONS = 6 };
+// Jeffrey Hogan - CS4379B - Spring 2015
+// General layout of code and examples copied from QT documentation at:
+// http://qt-project.org/doc/qt-4.8/layouts-basiclayouts.html
+#ifndef DIALOG_H
+  #define DIALOG_H
   
+  #include <QDialog>
+  #include <QList>
+  #include <QString>
+  class QRadioButton;
+  class QSlider;
+  class QDial;
+  class QProgressBar;
+  class QLCDNumber;
+  class QComboBox;
+  class QTabBar;
+  class QCheckBox;
+  class QAction;
+  class QDialogButtonBox;
+  class QGroupBox;
+  class QLabel;
+  class QLineEdit;
+  class QMenu;
+  class QMenuBar;
+  class QPushButton;
+  class QTextEdit;
+
+  class Dialog : public QDialog
+  {
+    Q_OBJECT
+    
+    public:
+    Dialog();
+    private slots:
+    void UpdateMethodLineEdit();
+    void UpdateBrightnessBar();
+    void UpdateColorTabBar();
+    void UpdatePortraitCheckbox();
+    void UpdateLandscapeCheckbox();
+    private:
+    void createMenu();
+    void createMethodGroupBox();
+    void createImageSizeGroupBox();
+    void createImageBrightnessGroupBox();
+    void createColorGroupBox();
+    void createLandscapeGroupBox();
+    void setMethodEdit(int id);
+    
+    enum { NumGridRows = 3, NUM_BUTTONS = 6 };
+    
     QList<QString> methodLabels;
-     QMenuBar *menuBar;
-     QRadioButton *methodButtons[NUM_BUTTONS];
-     QGroupBox *methodGroupBox;
-     QGroupBox *imageSizeGroupBox;
+    QList<QString> colorRepOptions;
+        
+    QGroupBox *methodGroupBox;  
+    QRadioButton *methodButtons[NUM_BUTTONS];    
+    QLineEdit *methodEdit;
+    
+    QGroupBox *imageSizeGroupBox;
     QDial *sizeDial;
-     QLCDNumber *imageSizeLCD;
-     QGroupBox *imageBrightnessGroupBox;
-QSlider *brightnessSlider;
+    QLCDNumber *imageSizeLCD;
+    
+    QGroupBox *imageBrightnessGroupBox;
+    QSlider *brightnessSlider;
     QProgressBar *brightnessProgressBar;
-     QGroupBox *colorGroupBox;
-     QGroupBox *landscapeGroupBox;
-  QLineEdit *methodEdit;
+    
+    QGroupBox *colorGroupBox;
+    QComboBox *colorRepComboBox;     
+    QTabBar *colorRepTabBar;
+    
+    QGroupBox *landscapeGroupBox;    
+    QPushButton *portraitBtn;
+    QPushButton *landscapeBtn;
+    QCheckBox *portraitCheckBox;
+    QCheckBox *landscapeCheckBox;
 
-  QComboBox *colorRepComboBox;     
-  QTabBar *colorRepTabBar;
-     QDialogButtonBox *buttonBox;
-
-     QMenu *fileMenu;
-     QAction *exitAction;
- };
-
- #endif
+    QMenuBar *menuBar;
+    QDialogButtonBox *buttonBox;    
+    QMenu *fileMenu;
+    QAction *exitAction;
+  };
+  
+#endif
